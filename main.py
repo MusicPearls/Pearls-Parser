cef7980d26d74a10fe91b4772e562aca0b405691
Leo-Cal <calasans.leonardo@gmail.com>
2025-01-09 11:19:37 -0300

    Updates main

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import SpotifyScraper.getTrackDetails
 import SpotifyScraper.getTracks
 import SpotifyScraper.utils
 import Parsers.AiParser
+import Parsers.RegexParser
 from dotenv import load_dotenv
 import os
 
 
 def main():
 
-    print(' 1: updateComposers \n 2: getAlbums \n 3: getTracks \n 4: getTracksDetails \n 5: Create OpenAI Batches \n 6: Upload OpenAI Batches \n 7: Check batches \n 8: Handle finished batches')
+    print(' 1: updateComposers \n 2: getAlbums \n 3: getTracks \n 4: getTracksDetails \n 5: Initial Regex Parse \n 6: Create OpenAI Batches \n 7: Start Uploading OpenAI Batches')
     option = int(input("Choose function: "))
     if option == 1:
         SpotifyScraper.getComposerInfo.updateComposerInfo()
@@ -22,12 +23,12 @@ def main():
     if option == 4:
         SpotifyScraper.getTrackDetails.updateComposerTrackDetails()
     if option == 5:
-        Parsers.AiParser.batchCreator()
+        opus_handler = Parsers.RegexParser.RegexParser()
+        opus_handler.getTracks()
+        opus_handler.parseOpus()
     if option == 6:
-        Parsers.AiParser.queueBatches()
+        Parsers.AiParser.batchCreator()
     if option == 7:
-        Parsers.AiParser.getBatches()
-    if option == 8:
-        Parsers.AiParser.handleFinishedBatches()
-main()
+        Parsers.AiParser.manageBatches()
 
+main()
